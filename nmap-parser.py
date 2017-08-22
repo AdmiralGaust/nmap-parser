d59f0b1bc0d95308d0f2a75ac4201d077b8cde00
Shiva Gupta <admiralgaust@gmail.com>
2017-08-22 12:30:37 +0530

    Update nmap-parser.py

--- nmap-parser.py
+++ nmap-parser.py
@@ -13,35 +13,47 @@ except:
 #Fill this list with  keywords you want to search for
 keyword_list=["sweet32","rc4","cbc","rsa"]
 
-one_ip_result = re.split('Nmap scan report for',data)
-keyword_port_dict = {}
-keyword_port = {}
-result = {}
 
+#This Function return the data of all ports as a list, the len of list is being equal to the number of ports  
+def find_port_data(data):
+    all_ports_data = re.split('SERVICE',data)
+    all_ports_data = all_ports_data[1]
+    all_ports_data=all_ports_data.splitlines()
+    all_ports_data.reverse()
+    one_port_data = ''
+    ports_data = []
+    num = ['0','1','2','3','4','5','6','7','8','9']
+    for i in all_ports_data:
+        if i.startswith(tuple(num)):
+            one_port_data +=i
+            ports_data.append(one_port_data)
+            one_port_data=''
+        else:
+            one_port_data+=i
+    return ports_data
+
+
+#Global variables
+one_ip_result = re.split('Nmap scan report for',data)
+
+print"\n\n"
+print " "*6 + "-"*55
 for data in one_ip_result:
-    port_data = re.split('SERVICE',data)
     try:
         ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',data).group()
     except:
         continue
-    for p in port_data:
-        try:
-            port = re.search(r'\d*/',p).group()
+
+    ports_data = find_port_data(data)
+
+    for port_data in ports_data:
+        if re.search(r'\d{1,3}/',port_data):
+            port = re.search(r'\d*/',port_data).group()
             port = port[:-1]
-        except:
-            continue
-        for keyword in keyword_list:
-            if re.search(keyword,p,re.I):
-                keyword_port[keyword]=port
-        keyword_port_dict[port] = keyword_port
-    result[ip] = keyword_port_dict
+            for keyword in keyword_list:
+                if re.search(keyword,port_data,re.I):
+                    print " "*6 + "| %20s | %5s | %20s |" %(ip,port,keyword)
+                    print" " *6 + "-"*55
 
-
-print" "*4  + "_"*64
-for i in result:
-    for j in result[i]:
-        for k in result[i][j]:
-            print " "*4 + "| " + i +" "*(19-len(i))+ "| " + j + " "*(19-len(j)) + "| " + k + " "*(19-len(k)) + "|"
-    print " "*4 + "|" + "_"*62 + "|"
 
 
